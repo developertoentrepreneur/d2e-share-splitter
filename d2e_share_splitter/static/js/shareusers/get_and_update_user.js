48ce72510ad44dd09fc9b503f22544a82341d361
Alavaro <alvaro@oneragtime.com>
2021-12-01 19:51:45 +0100

    loading working

--- d2e_share_splitter/static/js/shareusers/get_and_update_user.js
+++ d2e_share_splitter/static/js/shareusers/get_and_update_user.js
@@ -1,7 +1,10 @@
 function getUserForm(id) {
+  let loading = new bootstrap.Modal(document.getElementById("modalLoading"));
+  loading.show();
   let url = url_user_form.replace("0000", id);
   getRequest(url).then(
     (response) => {
+      loading.hide();
       $("#modalsContainer").html(response);
       console.log("userFormModalUpdate");
       var modalUpdate = new bootstrap.Modal(
