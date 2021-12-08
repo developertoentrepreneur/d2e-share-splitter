aa3870aa28146efd52ed6aa0ea0ecce5109f590c
Alavaro <alvaro@oneragtime.com>
2021-12-08 12:22:50 +0100

    Form semi finished

--- d2e_share_splitter/sharecontributions/forms.py
+++ d2e_share_splitter/sharecontributions/forms.py
@@ -3,21 +3,39 @@ from typing import Dict
 
 from django import forms
 
+from d2e_share_splitter.shareconf.models import Project
 from d2e_share_splitter.sharecontributions.models import Contribution
 from d2e_share_splitter.sharecontributions.utils import compute_pie_slices
+from d2e_share_splitter.users.models import User
+
+contribution_type_choices = (
+    ("time", "Time"),
+    ("expenses", "Expenses"),
+)
 
 
 class FormCreateContribution(forms.ModelForm):
+    details = forms.CharField(widget=forms.Textarea)
+    user = forms.ModelChoiceField(queryset=User.objects.all())
+    contribType = forms.ChoiceField(
+        choices=contribution_type_choices,
+    )
+    projectType = forms.ModelChoiceField(
+        queryset=Project.objects.all(),
+    )
+    hours = forms.TimeField()
+    date = forms.DateField(widget=forms.SelectDateWidget)
+
     class Meta:
         model = Contribution
         fields = [
+            "details",
+            "user",
             "contribType",
             "projectType",
             "value",
             "hours",
             "date",
-            "details",
-            "slices",
         ]
 
     def clean(self) -> Dict[str, Any]:
